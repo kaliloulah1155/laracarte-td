6f4f517b69a3da2170e2a411c4f0c283f81db971
kaliloulah1155 <ibrahim1155@outlook.com>
2018-05-30 17:03:09 +0000

    Setup and add static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="navbar navbar-default navbar-static-bottom">
+    <div class="container">
+    	<div class="text-center">
+    		 <p>&copy; {{date('Y')}} - Laracarte by @estsmo - A Laramap clone app.</p>
+        	<p><b>This app has been built for learning purposes.</b></p>
+    	</div>
+    </div>
+ </footer>
